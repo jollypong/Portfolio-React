222e786510531da25003687b6c9e86e8d557fb8b
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-25 00:26:56 -0600

    changed code to use project to reflect portfolio components

--- src/component/projects/Projects.jsx
+++ src/component/projects/Projects.jsx
@@ -7,140 +7,86 @@ import Project4 from "../../assets/images/projects/whoWorksWhere.png";
 import Project5 from "../../assets/images/projects/bate.png";
 import Project6 from "../../assets/images/projects/weatherDashboard.png";
 
+const data = [
+  {
+    id: 1,
+    image: Project1,
+    title: "Salbot AI Tutor",
+    github: "https://github.com/jollypong/SalBot",
+    demo: "https://jollypong.github.io/SalBot/",
+  },
+  {
+    id: 2,
+    image: Project2,
+    title: "Gaggle Chat Service",
+    github: "https://github.com/jollypong/Gaggle",
+    demo: "https://gagglechat.herokuapp.com/",
+  },
+  {
+    id: 3,
+    image: Project3,
+    title: "Quacker - Social Media Framework",
+    github: "https://github.com/jollypong/Gaggle",
+    demo: "",
+  },
+  {
+    id: 4,
+    image: Project4,
+    title: "Who Works Where? - CLI-based CMS",
+    github: "https://github.com/jollypong/Who-Works-Where",
+    demo: "",
+  },
+  {
+    id: 5,
+    image: Project5,
+    title: "Bate - Best All-time Text Editor",
+    github: "https://github.com/jollypong/BATE-Text-Editor",
+    demo: "https://bestalltimetexteditor.herokuapp.com/",
+  },
+  {
+    id: 6,
+    image: Project6,
+    title: "Weather Dashboard",
+    github: "https://github.com/jollypong/Weather-Dashboard",
+    demo: "https://jollypong.github.io/Weather-Dashboard/",
+  },
+];
+
 const Projects = () => {
   return (
     <section id="projects">
       <h5>My Projects</h5>
       <h2>Portfolio</h2>
+
       <div className="container portfolio_container">
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project1} alt="" />
-          </div>
-          <h3>Salbot AI Tutor</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/SalBot"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-            <a
-              href="https://jollypong.github.io/SalBot/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </div>
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project2} alt="" />
-          </div>
-          <h3>Gaggle Chat Service</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/Gaggle"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-            <a
-              href="https://gagglechat.herokuapp.com/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </div>
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project3} alt="" />
-          </div>
-          <h3>Quacker - Social Media Framework</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/Gaggle"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-          </div>
-        </div>
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project4} alt="" />
-          </div>
-          <h3>Who Works Where? - CLI-based CMS</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/Who-Works-Where"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-          </div>
-        </div>
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project5} alt="" />
-          </div>
-          <h3>Bate - Best All-time Text Editor</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/BATE-Text-Editor"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-            <a
-              href="https://bestalltimetexteditor.herokuapp.com/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </div>
-        <div className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={Project6} alt="" />
-          </div>
-          <h3>Weather Dashboard</h3>
-          <div className="buttonContainer">
-            <a
-              href="https://github.com/jollypong/Weather-Dashboard"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repo
-            </a>
-            <a
-              href="https://jollypong.github.io/Weather-Dashboard/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </div>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <div key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="buttonContainer">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github Repo
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
